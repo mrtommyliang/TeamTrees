5b8ec7e301687a3bd6018ba509dd3fbdbce9884c
Tommy Liang <tommyliangsays@gmail.com>
2019-11-06 11:41:03 -0500

    working on banner

--- teamtrees/src/App.js
+++ teamtrees/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
+import Banner from './Components/Banner';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Banner/>
     </div>
   );
 }
---
+++ teamtrees/src/Components/Banner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import bannerimg from '../images/banner.png'
+
+export default function Banner() {
+  return (
+    <div>
+      <img className="bannerimg" src={bannerimg}/>
+    </div>
+  )
+}
