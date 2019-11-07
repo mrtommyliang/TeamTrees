3bec07a9b668a53ff9713b46c80ab7996a3ea37d
Tommy Liang <tommyliangsays@gmail.com>
2019-11-07 17:52:13 -0500

    removed button, fixed sky color, added ttext

--- teamtrees/src/Components/Banner.jsx
+++ teamtrees/src/Components/Banner.jsx
@@ -7,6 +7,7 @@ export default function Banner() {
   return (
     <div className="bannerClass">
       <img className="bannerlogo" src={logo} />
+      <div className="helpplant">Help us plant 20 million trees around the globe by January 1st, 2020.</div>
       <img className="bannerimg" src={herobg}/>
     </div>
   )
